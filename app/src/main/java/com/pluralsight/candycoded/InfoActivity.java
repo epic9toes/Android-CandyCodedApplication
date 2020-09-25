bc983cb9b0514458fb3c61f1c60ae706e4550b16
Onwe Onweokoh-Nwaoji Chukwumaobim <kr8tiveonwe@gmail.com>
2020-09-25 02:55:43 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -38,6 +38,12 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
 
+    public void createPhoneIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        intent.setData(uri);
+        startActivity(intent);
+    }
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
